00c7ffe7c360231296af09eacd41e0f66cecbe68
Marcone M. Mendonça <marconemendonca@ymail.com>
2021-03-11 13:01:06 -0300

    Implementing the "fillTable ()".

--- scripts/play.js
+++ scripts/play.js
@@ -1,10 +1,19 @@
-import wordsColectionList from "./wordsDB.js";
+import { wordsColectionList, lettersCollection } from "./wordsDB.js";
 
-const raffledWords = [];
-for (let i = 0; i < 3; i++) {
-    const raffle = Math.round((Math.random() * 1000));
-    raffledWords.push(wordsColectionList[raffle])
-}
+const getRandomValue = limit =>
+    Math.round((Math.random() * limit));
+
+const getRandomValueIntoList = array =>
+    (getRandomValue(array.length - 1) - 1) + 1; // Getting a random line between 0 and ("table" length - 1).
+
+const raffleWords = (qty) => {
+    const result = [];
+    for (let i = 0; i < qty; i++) {
+        const raffle = getRandomValue(1000);
+        result.push(wordsColectionList[raffle])
+    }
+    return result;
+}// raffleWords(qty)
 
 const createTable = size => {
     const result = [];
@@ -22,10 +31,40 @@ const createTable = size => {
 
 const fillTable = () => {
     const table = createTable(10);
+    const raffledWordsList = raffleWords(3);
+    const raffledLinesList = [];
 
+    while (raffledLinesList.length < raffledWordsList.length) { //ensures that the "lines raffling" never will contains double values.
+        const raffledLine = getRandomValueIntoList(table);
 
-}
-const splitedWord = raffledWords[0].split("");
+        if (raffledLinesList.includes(raffledLine))
+            continue;
+        else
+            raffledLinesList.push(raffledLine);
+    }
 
+    for (let raffle = 0; raffle < raffledWordsList.length; raffle++) {
+
+        const line = raffledLinesList[raffle];
+        const splitedWord = raffledWordsList[raffle].split("");
+
+        table[line] = splitedWord;
+    }
+
+    for (let line = 0; line < table.length; line++) {
+        const word = table[line];
+        for (let column = 0; column < word.length; column++) {
+            if (word[column] === "") {
+                const randomValue = getRandomValueIntoList(lettersCollection);
+                word[column] = lettersCollection[randomValue];
+            }
+        }
+    }
+
+    console.log(table);
+
+}// fillTable()
+
+console.log(fillTable());
+// console.log((getRandomValue(9) - 1) + 1);
 // console.log(createTable(100));
-console.log(splitedWord);
