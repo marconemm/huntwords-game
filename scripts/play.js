3270fcfd913aaefe65c5a46cb9979868fa4ef4b9
Marcone M. Mendonça <marconemendonca@ymail.com>
2021-03-11 12:02:41 -0300

    Implementing the "createTable(size)".

--- scripts/play.js
+++ scripts/play.js
@@ -1,2 +1,31 @@
 import wordsColectionList from "./wordsDB.js";
 
+const raffledWords = [];
+for (let i = 0; i < 3; i++) {
+    const raffle = Math.round((Math.random() * 1000));
+    raffledWords.push(wordsColectionList[raffle])
+}
+
+const createTable = size => {
+    const result = [];
+
+    for (let line = 0; line < size; line++) {
+        const columList = [];
+        for (let column = 0; column < size; column++) {
+            columList.push("");
+        }
+        result.push(columList);
+    }
+
+    return result;
+}// createTable(size)
+
+const fillTable = () => {
+    const table = createTable(10);
+
+
+}
+const splitedWord = raffledWords[0].split("");
+
+// console.log(createTable(100));
+console.log(splitedWord);
